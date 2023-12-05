1098a940d36a34a4cf382da862a9875f70f8420e
yeonji-dev <lilianaaa804@gmail.com>
2023-12-05 17:07:05 +0900

    Test: Todo CRUD

--- src/test/java/com/yeonjidev/mallapi/repository/TodoRepositoryTests.java
+++ src/test/java/com/yeonjidev/mallapi/repository/TodoRepositoryTests.java
@@ -1,11 +1,13 @@
 package com.yeonjidev.mallapi.repository;
 
-import lombok.ToString;
+import com.yeonjidev.mallapi.domain.Todo;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @SpringBootTest
 @Log4j2
@@ -18,5 +20,44 @@ public class TodoRepositoryTests {
     public void test1(){
         log.info("---------------------------");
         log.info(todoRepository);
+    }
+
+    @Test
+    public void testInsert(){
+        for(int i = 1; i <= 100; i++){
+            Todo todo = Todo.builder()
+                    .title("Title..." + i)
+                    .dueDate(LocalDate.of(2023, 12, 31))
+                    .writer("user00")
+                    .build();
+
+            todoRepository.save(todo);
+        }
+    }
+
+    @Test
+    public void testRead(){
+        Long tno = 33L;
+        Optional<Todo> result = todoRepository.findById(tno);
+        Todo todo = result.orElse(null);
+        log.info(todo);
+    }
+
+    @Test
+    public void testModify(){
+        Long tno = 33L;
+        Optional<Todo> result = todoRepository.findById(tno);
+        Todo todo = result.orElse(null);
+        todo.changeTitle("Modified 33...");
+        todo.changeComplete(true);
+        todo.changeDueDate(LocalDate.of(2023,10,10));
+
+        todoRepository.save(todo);
+    }
+
+    @Test
+    public void testDelete(){
+        Long tno = 1L;
+        todoRepository.deleteById(tno);
     }
 }
