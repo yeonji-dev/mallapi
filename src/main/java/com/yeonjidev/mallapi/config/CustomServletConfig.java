3e752b6d96f8ceeb268a648ba49fd2bdba04f88a
yeonji-dev <lilianaaa804@gmail.com>
2023-12-27 11:49:27 +0900

    Config: CORS 설정

--- src/main/java/com/yeonjidev/mallapi/config/CustomServletConfig.java
+++ src/main/java/com/yeonjidev/mallapi/config/CustomServletConfig.java
@@ -3,6 +3,7 @@ package com.yeonjidev.mallapi.config;
 import com.yeonjidev.mallapi.controller.formatter.LocalDateFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,14 @@ public class CustomServletConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatter(new LocalDateFormatter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(60 * 5) // 5분 동안의 사전 요청의 결과를 캐시
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
     }
 }
